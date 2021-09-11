0dd2fe2213adf90f8c309e5a124931a756ffc650
gizemciritcii <gizemciritci58@gmail.com>
2021-09-12 01:04:11 +0300

    login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ username! : string;
+ password!: string;
   constructor() { }
 
   ngOnInit(): void {
